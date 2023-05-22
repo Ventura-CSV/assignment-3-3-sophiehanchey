ce112aeeca45bce7ec0df8c9ef030828e1991827
Kyu Lee <kwooler@gmail.com>
2023-05-22 01:57:21 -0700

    Assignment 3-3

--- main.py
+++ main.py
@@ -7,21 +7,6 @@ def main():
     """
     result = True
 
-    if not email[0].isalpha():
-        result = False
-
-    elen = len(email)
-    if elen <= 5 or elen >= 30:
-        result = False
-
-    atidx = email.find('@')
-    if atidx == -1:
-        result = False
-    else:
-        if email[atidx:].find('.') == -1:
-            result = False
-
-    print(result)
     ########################################
     # Do not delete the return statement
     ########################################
